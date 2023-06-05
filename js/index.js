ed6a7846aa34e56a11a48337d5c4ac38a13c7900
valeriizraz <valeriizraz@gmail.com>
2023-06-05 15:02:04 +0200

    правки

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+{
 // phone AppleX 
 const phoneAppleX = 'Apple X';
 const phoneAppleXQuantity = 15;
@@ -9,15 +10,15 @@ const phoneAppleXPrice = 999;
 console.log(typeof phoneAppleXQuantity);
 console.log(typeof phoneAppleXPrice);
 console.log(`На складе осталось (${phoneAppleXQuantity}) единиц товара (${phoneAppleX}) на сумму (${phoneAppleXQuantity * phoneAppleXPrice}) $`)
+}
 
 {
-  const tvLg = "TV LG";
-  let tvLgQuantity = 8;
-    tvLgQuantity = prompt('Ведите нужное количество', '')
-  const tvS = 'Телевизоры';
-  const tvPrice = 178;
+  const productName = prompt('Ведите название');
+  const productQuantity = prompt('Ведите количество');
+  const productCategory = prompt('Ведите категорию');
+  const productPrice = prompt('Ведите цену');
 
-  console.log(`на складе осталось (${tvLgQuantity}) единиц товара (${tvLg}) на сумму (${tvPrice * tvLgQuantity})`)
+  console.log(`на складе осталось (${productQuantity}) единиц товара на сумму (${productPrice * productQuantity})`)
 }
 
 
