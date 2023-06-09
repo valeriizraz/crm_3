59d88818f3350a64017a39f583ff30ef531209a1
valeriizraz <valeriizraz@gmail.com>
2023-06-09 15:48:51 +0200

    init project

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
 'use strict'
 
-{
+// {
 // phone AppleX 
-const phoneAppleX = 'Apple X';
-const phoneAppleXQuantity = 15;
-const cellPhones = 'Мобильные телефоны';
-const phoneAppleXPrice = 999;
+// const phoneAppleX = 'Apple X';
+// const phoneAppleXQuantity = 15;
+// const cellPhones = 'Мобильные телефоны';
+// const phoneAppleXPrice = 999;
 
-console.log(typeof phoneAppleXQuantity);
-console.log(typeof phoneAppleXPrice);
-console.log(`На складе осталось (${phoneAppleXQuantity}) единиц товара (${phoneAppleX}) на сумму (${phoneAppleXQuantity * phoneAppleXPrice}) $`);
-}
+// console.log(typeof phoneAppleXQuantity);
+// console.log(typeof phoneAppleXPrice);
+// console.log(`На складе осталось (${phoneAppleXQuantity}) единиц товара (${phoneAppleX}) на сумму (${phoneAppleXQuantity * phoneAppleXPrice}) $`);
+// }
 
-{
-  const productName = prompt('Ведите название');
-  const productQuantity = prompt('Ведите количество');
-  const productCategory = prompt('Ведите категорию');
-  const productPrice = prompt('Ведите цену');
+// {
+//   const productName = prompt('Ведите название');
+//   const productQuantity = Number(prompt('Ведите количество'));
+//   const productCategory = prompt('Ведите категорию');
+//   const productPrice = prompt('Ведите цену');
 
-  console.log(`на складе осталось (${productQuantity}) единиц товара на сумму (${productPrice * productQuantity})`);
-}
+//   console.log(`на складе осталось (${productQuantity}) единиц товара на сумму (${productPrice * productQuantity})`);
+// }
 
 
 
---
+++ js/tasks.js
@@ -0,0 +1,48 @@
+'use strict'
+
+// {
+//   const rain = Math.random();
+//   console.log(Math.round(rain));
+//   if (rain > 0.5) {
+//     console.log('Пошел дождь, возьмите зонт');
+//   } else {
+//     console.log('Дождя нет!');
+//   }
+// }
+
+// {
+//   const examMathematics = prompt('Ведите количество баллов по Математике');
+//   const examRussianLanguage = prompt('Ведите количество баллов по Русскому языку');
+//   const examInformatics = prompt('Ведите количество баллов по Информатике');
+
+//   if (examMathematics + examRussianLanguage + examInformatics >= 256) {
+//     console.log('Поздравляем! Вы отлично поработали!')
+//   } else {
+//     console.log('До следующего раза, неудачник!')
+//   }
+// }
+
+// {
+//   const bank = prompt('Ведите сумму которую Вам необходимо')
+//   if (bank % 100 === 0) {
+//     console.log('Возьмите свои грязные деньги!')
+//   } else {
+//     console.log('Терминал не выдает мелкие купюры, введите другую сумму')
+//   }
+// }
+
+// держи
+// function checkRating() {
+//   const min = 100
+//   const max = 5000
+//   return (number) => {
+//     if(number >= min && number <= max && number % 100 === 0) return true
+//     return false
+//   }
+// }
+
+// function getMoney(money) {
+//   const avalibleRating = checkRating()
+//   return avalibleRating(money) ? "Доступно" : "Нет в наличии"
+// }
+// console.log(getMoney(Number(prompt('Количество денег для снятия?',''))))
